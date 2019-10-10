be3001a5436e779f7d7ca76e83be5d84a4428452
krushnap <smitakpatil26@gmail.com>
2019-10-10 23:45:56 +0530

    start for functionality

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//display city details
+document.getElementById("list").addEventListener("keypress",function displayData(e){
+    if(e.keyCode==13)
+    {
+        console.log("kegjd")
+        
+    }
+
+})
