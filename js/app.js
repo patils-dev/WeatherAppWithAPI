8c8706daf171f4aeb1776b089bafc87eaa4f5bcb
krushnap <smitakpatil26@gmail.com>
2019-10-11 23:39:29 +0530

    displaying date-time

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 //display city details
-document.getElementById("list").addEventListener("keypress",function displayData(e){
-    if(e.keyCode==13)
-    {
-        console.log("kegjd")
-        
-    }
 
-})
+// window.onload()
+// {
+    document.getElementById("list").addEventListener("keypress",function displayData(e){
+        if(e.keyCode==13)
+        {
+            console.log("kegjd")
+            let result=document.getElementById("list").value;
+            dw =new Weather(result);
+            dw.displayDate()
+            dw.displayValues();
+        }
+    
+    })
+// }
--- js/class.js
+++ js/class.js
@@ -3,10 +3,16 @@ class Weather{
     {
         this.city=city;
     }
-}
 
+    displayDate()
+    {
+        var d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
+        var newD = new Date();
+        var n = newD.getDay();
+        let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+        document.getElementById("day").innerHTML =week[n]+" , "+d;
 
-
+    }
 displayValues(){
     let selectedCity=this.city;
 
@@ -14,7 +20,6 @@ displayValues(){
     $.getJSON("http://api.openweathermap.org/data/2.5/find?q="+selectedCity+"&units=metric&appid=344b790f76ac39d29c9973f2736c19c3", 
     function(data){
         let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
-        
     })
 }
 
