f2813310493883ba9d6867676a732e2c4eaed403
krushnap <smitakpatil26@gmail.com>
2019-10-14 22:07:50 +0530

    added image

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function init()
     document.getElementById("list").addEventListener("keypress",function displayData(e){
         if(e.keyCode==13)
         {
-            console.log("kegjd")
+            // console.log("kegjd")
             let result=document.getElementById("list").value;
             dw =new Weather(result);
             dw.displayDate()
