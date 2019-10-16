e189a606dca8ea5dc9c84e75f50bca21c5951679
krushnap <smitakpatil26@gmail.com>
2019-10-16 18:32:17 +0530

    removed comments

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 //display city details
-
 function init()
 {
     document.getElementById("list").addEventListener("keypress",function displayData(e){
         if(e.keyCode==13)
         {
-            // console.log("kegjd")
             let result=document.getElementById("list").value;
             dw =new Weather(result);
             dw.displayDate()
             dw.displayValues();
         }
     })
+
     document.getElementById("fahrenheit").addEventListener("click",function displayFah(e){
 
         let result=document.getElementById("list").value;
         dw =new Weather(result);
         dw.displayFahrenheit();
     })
+
     document.getElementById("celcius").addEventListener("click",function displayFah(e){
 
         let result=document.getElementById("list").value;
--- js/class.js
+++ js/class.js
@@ -11,43 +11,40 @@ class Weather{
         var n = newD.getDay();
         let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
         document.getElementById("day").innerHTML =week[n]+" , "+d;
-
     }
-displayValues(){
-    let selectedCity=this.city;
 
-    // const proxy="https://cors-anywhere.herokupp.com/"
-    $.getJSON("http://api.openweathermap.org/data/2.5/find?q="+selectedCity+"&units=metric&appid=344b790f76ac39d29c9973f2736c19c3", 
-        function(data){
-            let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
-            document.getElementById("cityName").innerHTML=data.list[0].name;
-            document.getElementById("stateName").innerHTML=data.list[0].sys.country;
-            $("#icon").attr("src",icon);
-            document.getElementById("weatherName").innerHTML=data.list[0].weather[0].main;
-            document.getElementById("temperature").innerHTML=data.list[0].main.temp;
-            document.getElementById("celcius").disabled = true;
-        })
-}
+    displayValues()
+    {
+        let selectedCity=this.city;
+        $.getJSON("http://api.openweathermap.org/data/2.5/find?q="+selectedCity+"&units=metric&appid=344b790f76ac39d29c9973f2736c19c3", 
+            function(data){
+                let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
+                document.getElementById("cityName").innerHTML=data.list[0].name;
+                document.getElementById("stateName").innerHTML=data.list[0].sys.country;
+                $("#icon").attr("src",icon);
+                document.getElementById("weatherName").innerHTML=data.list[0].weather[0].main;
+                document.getElementById("temperature").innerHTML=data.list[0].main.temp;
+                document.getElementById("celcius").disabled = true;
+            })
+    }
 
-displayFahrenheit(){
-    let tempCel=document.getElementById("temperature").textContent;
-    let fahTemp=Math.round((tempCel*1.8)+32);
-    document.getElementById("fahrenheit").disabled = true;
-    document.getElementById("celcius").disabled = false;
-    //console.log(fahTemp);
-    document.getElementById("temperature").innerHTML=fahTemp;
+    displayFahrenheit(){
+        let tempCel=document.getElementById("temperature").textContent;
+        let fahTemp=Math.round((tempCel*1.8)+32);
+        document.getElementById("fahrenheit").disabled = true;
+        document.getElementById("celcius").disabled = false;
+        //console.log(fahTemp);
+        document.getElementById("temperature").innerHTML=fahTemp;
+    }
 
-}
-
-displayCelcius(){
-
-    let fahTemp=document.getElementById("temperature").textContent;
-    // let celTemp=Math.round((fahTemp*1.8)+32);
-    let celTemp=Math.round((fahTemp-32) * 5/9 );
-    document.getElementById("celcius").disabled = true;
-    document.getElementById("fahrenheit").disabled= false;
-    document.getElementById("temperature").innerHTML=celTemp;
-
-}
+    displayCelcius()
+    {
+        let fahTemp=document.getElementById("temperature").textContent;
+        // let celTemp=Math.round((fahTemp*1.8)+32);
+        let celTemp=Math.round((fahTemp-32) * 5/9 );
+        document.getElementById("celcius").disabled = true;
+        document.getElementById("fahrenheit").disabled= false;
+        document.getElementById("temperature").innerHTML=celTemp;
+    }
 
 }
