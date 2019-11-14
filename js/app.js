ad1fa823cc7a89089be8f09ac0f3401959dae2b1
smita patil <smitakpatil26@gmail.com>
2019-11-14 23:36:24 +0530

    added comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-//display city details
 function init()
 {
+    //this function will excecute after pressing enter key and it will get the data which is entered.
     document.getElementById("list").addEventListener("keypress",function displayData(e){
         if(e.keyCode==13)
         {
@@ -11,14 +11,14 @@ function init()
             dw.displayValues();
         }
     })
-
+    //this function will excecute after clicking on farenheit button and it will convert temperature in Fahrenheit unit.
     document.getElementById("fahrenheit").addEventListener("click",function displayFah(e){
 
         let result=document.getElementById("list").value;
         dw =new Weather(result);
         dw.displayFahrenheit();
     })
-
+    //this function will excecute after clicking on celcius button and it will convert temperature in Celcius unit.
     document.getElementById("celcius").addEventListener("click",function displayFah(e){
 
         let result=document.getElementById("list").value;
